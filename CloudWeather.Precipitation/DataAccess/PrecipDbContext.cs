c8c8960811a40411d744d82d779ed8b833c8d89a
Midgard <midgardsaidamador@gmail.com>
2025-03-12 16:52:39 -0600

    add data access to precipitation

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+	public class PrecipDbContext : DbContext
+	{
+		public PrecipDbContext() { }
+		public PrecipDbContext(DbContextOptions opts) : base(opts) { }
+
+		public DbSet<Precipitation> Precipitation { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			SnakeCaseIdentityTableNames(modelBuilder);
+		}
+
+		private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Precipitation>(b => { b.ToTable("precipitation"); });
+		}
+	}
+}
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,14 +1,36 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
+builder.Services.AddDbContext<PrecipDbContext>(
+	opts =>
+	{
+		opts.EnableSensitiveDataLogging();
+		opts.EnableDetailedErrors();
+		opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+	},
+	ServiceLifetime.Transient
+);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
-{
-	return Results.Ok(zip);
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>{
+	if (days == null || days < 1 || days > 30)
+	{
+		return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+	}
+	var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+	var results = await db.Precipitation.Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate).ToListAsync();
+
+	return Results.Ok(results);
+	
 });
 
 app.Run();
