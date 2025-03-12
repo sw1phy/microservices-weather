713f0278c4fc95a96554f9cd7bc70b670b32472c
Midgard <midgardsaidamador@gmail.com>
2025-03-12 13:32:49 -0600

    Add initial deps

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 var app = builder.Build();
 
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+{
+	return Results.Ok(zip);
+});
 
 app.Run();
