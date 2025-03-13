7ea656b4eb6b4c02557a538b9adeab9ecb82d05d
Midgard <midgardsaidamador@gmail.com>
2025-03-13 13:52:33 -0600

    Add endpoints for services

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -33,4 +33,11 @@ app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, Preci
 	
 });
 
+app.MapPost("/observation", async (Precipitation precip, PrecipDbContext db) =>
+{
+	precip.CreatedOn = precip.CreatedOn.ToUniversalTime();
+	await db.AddAsync(precip);
+	await db.SaveChangesAsync();
+});
+
 app.Run();
--- CloudWeather.Temperature/Program.cs
+++ CloudWeather.Temperature/Program.cs
@@ -1,4 +1,5 @@
 using CloudWeather.Temperature.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,29 @@ builder.Services.AddDbContext<TemperatureDbContext>(
 
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, TemperatureDbContext db) => {
+	if (days == null || days < 1 || days > 30)
+	{
+		return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+	}
+	var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+	var results = await db.Temperature.Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate).ToListAsync();
+
+	return Results.Ok(results);
+
+});
+
+app.MapPost("/observation", async (Temperature temperature, TemperatureDbContext db) =>
+{
+	temperature.CreatedOn = temperature.CreatedOn.ToUniversalTime();
+	await db.AddAsync(temperature);
+	await db.SaveChangesAsync();
+});
+
+
+
+
 
 
 app.Run();
