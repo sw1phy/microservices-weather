0d67bb1ffc43271d0f1abfd6b70e03b39fbb9d4c
Midgard <midgardsaidamador@gmail.com>
2025-03-12 20:40:09 -0600

    Add data access for remaining microservices

--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,6 +1,18 @@
+using CloudWeather.Report.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<WeatherReportDbContext>(
+	opts =>
+	{
+		opts.EnableSensitiveDataLogging();
+		opts.EnableDetailedErrors();
+		opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+	},
+	ServiceLifetime.Transient
+);
 
 var app = builder.Build();
 
--- CloudWeather.Temperature/Program.cs
+++ CloudWeather.Temperature/Program.cs
@@ -1,6 +1,20 @@
+using CloudWeather.Temperature.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<TemperatureDbContext>(
+	opts =>
+	{
+		opts.EnableSensitiveDataLogging();
+		opts.EnableDetailedErrors();
+		opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+	},
+	ServiceLifetime.Transient
+);
+
 
 var app = builder.Build();
 
