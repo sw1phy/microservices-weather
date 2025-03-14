1958c6c63e7a184ecb1d492082ff1de74d5b6edb
Midgard <midgardsaidamador@gmail.com>
2025-03-14 15:30:38 -0600

    Add weather report microservices logic

--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,7 +1,15 @@
+using CloudWeather.Report.BusinessLogic;
+using CloudWeather.Report.Config;
 using CloudWeather.Report.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<IWeatherReportAggregator, WeatherReportAggregator>();
+builder.Services.AddOptions();
+builder.Services.Configure<WeatherDataConfig>(builder.Configuration.GetSection("WeatherDataConfig"));
 
 
 builder.Services.AddDbContext<WeatherReportDbContext>(
@@ -15,6 +23,20 @@ builder.Services.AddDbContext<WeatherReportDbContext>(
 );
 
 var app = builder.Build();
+
+
+app.MapGet(
+	"/weather-report/{zip}", 
+	async (string zip, [FromQuery] int? days, IWeatherReportAggregator weatherAgg) =>
+{
+	if (days == null || days > 30 || days < 1)
+	{
+		return Results.BadRequest("Please provide a 'days' query parameter with a value between 1 and 30");
+	}
+	var report = await weatherAgg.BuildReport(zip, days.Value);
+	return Results.Ok(report);
+
+});
 
 
 app.Run();
